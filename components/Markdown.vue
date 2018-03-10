18f0556a7ed115039386e1464290220df7233598
Scott Moss <willscottmoss@gmail.com>
2018-03-10 13:25:33 -0800

    💄 add code block styles

--- components/Markdown.vue
+++ components/Markdown.vue
@@ -6,6 +6,23 @@ import Marked from 'marked'
 import HighlightJs from 'highlight.js'
 import Clicker from './Clicker.vue'
 import Vue from 'vue'
+const renderer = new Marked.Renderer()
+
+renderer.code = function(code, lang) {
+  console.log(code)
+  console.log(lang)
+  return `
+    <div class="code-sample">
+      <div class="lang-name">
+        <span>${lang}</span>
+      </div>
+      <div class="code">
+        <pre class="code-pre"><code class="${lang} hljs">${this.options.highlight(code, lang)}</code></pre>
+      </div>
+    </div>
+  `
+}
+
 const componentSelectors = [
   'graphiql',
   'box'
@@ -19,6 +36,7 @@ export default {
   computed: {
     content () {
       Marked.setOptions({
+        renderer,
         gfm: true,
         highlight: function (code) {
           return HighlightJs.highlightAuto(code).value
@@ -34,7 +52,7 @@ export default {
   head () {
     return {
       link: [
-        {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atom-one-light.min.css'},
+        {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/tomorrow-night-eighties.min.css'},
         {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css'}
       ]
     }
@@ -61,3 +79,23 @@ export default {
   }
 }
 </script>
+<style lang="stylus">
+  @require '~assets/theme/colors.styl'
+  bgcolor = rgb(53,55,66)
+  gradient = linear-gradient(73deg, lighten(bgcolor, 10%), bgcolor)
+  .code-sample
+    pre
+      background-image gradient
+      background-color bgcolor
+      border-radius 0px 0px 3px 3px
+      code
+        background-color transparent
+
+    .lang-name
+      color white
+      padding 5px 1.5em
+      background-color bgcolor
+      background-image gradient
+      border-radius 3px 3px 0px 0px
+      border-bottom 1px solid color-light
+</style>
