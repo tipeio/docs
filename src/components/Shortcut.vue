3bf67fb5bfee06ab466266bf03d263dcf7e3c7c5
Scott Moss <hendrixer@Moby.local>
2019-10-22 18:46:51 -0700

    chore(): init

---
+++ src/components/Shortcut.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="shortcut">
+    <h2>
+      <g-link :to="link">
+        <component :is="icon"></component>
+        {{ text }}
+      </g-link>
+    </h2>
+  </div>
+</template>
+
+<script>
+import {
+  PlayIcon,
+  GlobeIcon,
+  CodeIcon,
+  NavigationIcon,
+  TerminalIcon,
+  ZapIcon
+} from "vue-feather-icons";
+export default {
+  components: {
+    ZapIcon,
+    PlayIcon,
+    GlobeIcon,
+    CodeIcon,
+    NavigationIcon,
+    TerminalIcon
+  },
+  props: {
+    link: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.shortcut {
+  border: 1px solid shade($sidebarBright, 10%);
+  background: $sidebarBright;
+  padding: 2rem;
+  border-radius: 3px;
+  transition: background 0.15s ease-in-out, border-color 0.15s ease-in-out;
+  width: 100%;
+  height: 100%;
+
+  .dark & {
+    border: 1px solid shade($sidebarDark, 10%);
+    background: $sidebarDark;
+  }
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+svg {
+  width: 2em;
+  height: 2em;
+  margin-bottom: 1em;
+
+  @include respond-above(md) {
+    width: 3em;
+    height: 3em;
+  }
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <Layout :sidebar="false">
+    <div class="content">
+      <h1>Tipe Docs</h1>
+      <nav>
+        <div class="col">
+          <Shortcut link="/getting-started" text="Getting Started" icon="play-icon" />
+        </div>
+        <div class="col">
+          <Shortcut link="/theme-configuration" text="Guides" icon="navigation-icon" />
+        </div>
+        <div class="col">
+          <Shortcut link="/theme-configuration" text="SDKS" icon="terminal-icon" />
+        </div>
+        <div class="col">
+          <Shortcut
+            link="/theme-configuration#changing-colors"
+            text="API Reference"
+            icon="globe-icon"
+          />
+        </div>
+        <div class="col">
+          <Shortcut
+            link="/theme-configuration#changing-colors"
+            text="Starter Repos"
+            icon="code-icon"
+          />
+        </div>
+        <div class="col">
+          <Shortcut
+            link="/theme-configuration#changing-colors"
+            text="Built with Tipe"
+            icon="zap-icon"
+          />
+        </div>
+      </nav>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Shortcut from "~/components/Shortcut.vue";
+
+export default {
+  components: {
+    Shortcut
+  },
+  metaInfo: {
+    title:
+      "The ultimate static generated documentation theme for the JAM-stack",
+    meta: [
+      {
+        key: "description",
+        name: "description",
+        content:
+          "A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click."
+      }
+    ]
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+}
+
+h1,
+h3 {
+  text-align: center;
+  max-width: 600px;
+  margin: 1.5em auto 1.5em;
+
+  @include respond-above(md) {
+    max-width: 1000px;
+  }
+}
+
+h2 {
+  font-size: 1.4rem;
+  margin: 0;
+}
+
+nav {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+
+nav .col {
+  flex: 0 0 auto;
+  width: 100%;
+  padding: 0.5rem;
+
+  @include respond-above(sm) {
+    width: 33%;
+  }
+}
+
+.git {
+  margin: 3em 0 0;
+  align-self: center;
+
+  @include respond-above(md) {
+    margin: 5em 0 0;
+  }
+}
+</style>
